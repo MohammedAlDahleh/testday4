cadc5ca7e532f72d593420c8e16b6fb03f326ce8
Mohammed AlDahleh <mohammedd993@gmail.com>
2024-04-29 19:57:34 +0000

    HTML Part

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict'
+// console.log("hello world");
+
+// var name = "mohammed";
+// console.log(name);
+// alert("Welcome "+name+" to js ")
+// var userAnswer = confirm("are you sure?");
+// //console.log(userAnswer);
+// var userName = prompt("what is your name ?");
+// console.log("welcome to our website " + userName);
+// console.log(`welcome again ${userName} to our js `);
+// console.log(typeof userName);
+
+// if(userName.toLocaleLowerCase()==="ali"){
+//     console.log("hello Ali");
+// }else{
+//     console.log("hello "+userName)
+// } 
+
+// (userName.toLocaleLowerCase()==="ali")? console.log("hello Ali"):console.log("hi "+userName)
+// var userName = "ali";
+// console.log(userName);
